8179dc7e1eb5184b54c3059241d1ae31bedef4c6
Валери Ковалева <kovaleva.valeri71@gmail.com>
2023-05-15 17:39:48 +0300

    SRP

---
+++ src/Menu.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import './Menu.css';
+function Menu() {
+    return (
+        <div>
+
+            <a href="#"> Link to google </a>
+
+            <a href="#"> Link to yandex </a>
+
+        </div>
+    );
+}
+
+export default Menu;
